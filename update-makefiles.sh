0fba793fd9973d5a76a91979ea6d23b1109857ed
PortalRomOS <portalromOS@gmail.com>
2023-01-23 17:25:23 +0000

    change to portalrom

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -3,6 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.lineage:hardware/portalrom/interfaces" \
+  "vendor.portalrom:hardware/portalrom/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"
